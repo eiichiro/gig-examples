28e1a23c7b7bec05d89e72aefab51a0f4cd00577
Eiichiro Uchiumi <eiichiro.uchiumi@gmail.com>
2022-11-21 11:47:18 +0900

    Modify dependencies for JDK 17 build

---
+++ guestbook-webapp-appengine/.apt_generated/gig/examples/guestbook/_Comment.java
@@ -0,0 +1,23 @@
+package gig.examples.guestbook;
+
+import javax.annotation.Generated;
+import org.eiichiro.acidhouse.metamodel.Metamodel;
+
+@Generated("org.eiichiro.acidhouse.metamodel.processing.MetamodelGenerator")
+public class _Comment extends Metamodel<Comment> {
+
+	public _Comment() {
+		super(Comment.class);
+	}
+
+	public _Comment(Metamodel<?> parent, String name) {
+		super(parent, Comment.class, name);
+	}
+
+	public org.eiichiro.acidhouse.metamodel.ComparableProperty<Comment, java.lang.String> id = new org.eiichiro.acidhouse.metamodel.ComparableProperty<Comment, java.lang.String>(this, java.lang.String.class, "id");
+
+	public org.eiichiro.acidhouse.metamodel.ComparableProperty<Comment, java.lang.String> text = new org.eiichiro.acidhouse.metamodel.ComparableProperty<Comment, java.lang.String>(this, java.lang.String.class, "text");
+
+	public org.eiichiro.acidhouse.metamodel.ComparableProperty<Comment, java.util.Date> date = new org.eiichiro.acidhouse.metamodel.ComparableProperty<Comment, java.util.Date>(this, java.util.Date.class, "date");
+
+}
